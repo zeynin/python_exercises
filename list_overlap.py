4396ab638b1e4faf85c159211395f552ff3c4f05
Zeynin Juma <zeynin@hotmail.com>
2020-09-03 07:23:26 +0000

    random lists

--- list_overlap.py
+++ list_overlap.py
@@ -2,9 +2,15 @@
 from itertools import chain
 from itertools import combinations
 from collections import Counter 
+import random
 
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+#a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+b = random.sample(range(0, 50), random.randint(1,30))
+print(b)
+a = random.sample(range(0, 50), random.randint(1,30))
+print(a)
+
 z = []
 for a_same in a:
     for b_same in b:
