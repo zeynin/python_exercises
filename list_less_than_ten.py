926e8b092e0d6450942a6dd8cb41296f35f733ba
Zeynin Juma <zeynin@hotmail.com>
2020-09-03 03:13:34 +0000

    updated

--- list_less_than_ten.py
+++ list_less_than_ten.py
@@ -3,7 +3,11 @@
 print('hell0')
 
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+l = []
 
 less_than_ten = lambda x: x < 10
 for val in filter(less_than_ten, a):
     print(val)
+    l.append(val)
+
+print(l)
