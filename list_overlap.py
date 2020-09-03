d26dbf0ef1724efa50d07f655bb2fff7f01cb6bd
Zeynin Juma <zeynin@hotmail.com>
2020-09-03 06:48:07 +0000

    one liner achieved

---
+++ list_overlap.py
@@ -0,0 +1,23 @@
+#http://www.practicepython.org/exercise/2014/03/05/05-list-overlap.html
+from itertools import chain
+from itertools import combinations
+from collections import Counter 
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+z = []
+for a_same in a:
+    for b_same in b:
+        if( a_same == b_same ):
+            z.append(b_same)
+
+z = [*set(z)]
+print(z)
+
+#y = [set(chain(a,b))] #not quite
+#y = [set(b) - set(a)] #not quite
+y = [*set(same for same in a + b if same in a and same in b)]
+print(y)
+
+#x = [set(a + b)] #nope
+#print(x)
