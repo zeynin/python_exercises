f20dc95e7f4737973a52ded34a09044dbd9d0cf0
Zeynin Juma <zeynin@hotmail.com>
2020-09-03 03:08:01 +0000

    new exercise

---
+++ list_less_than_ten.py
@@ -0,0 +1,9 @@
+#http://www.practicepython.org/exercise/2014/02/15/03-list-less-than-ten.html
+
+print('hell0')
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+less_than_ten = lambda x: x < 10
+for val in filter(less_than_ten, a):
+    print(val)
