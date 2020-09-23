2bb8b2bdb391fc0c29bced93b01b1aeafc860f44
Zeynin Juma <zeynin@hotmail.com>
2020-09-23 03:57:38 +0000

    completed & works

--- Rock_paper_scissors.py
+++ Rock_paper_scissors.py
@@ -5,34 +5,54 @@ Paper beats rock'''
 
 player1 = ''
 player2 = ''
+ESC = chr(27) #0x1b
 
-while( (player1 or player2) != 'esc' ):
+def player1Test( x ):
+    b = x in player1
+    return b 
+
+def player2Test( x ):
+    b = x in player2
+    return b 
+
+while( 1 ):
     player1 = input("PLAYER 1: What is your play? Enter rock, paper or scissors. ")
-    player2 = input("PLAYER 2: What is your play? Enter rock, paper or scissors. ")
+    if( player1Test(ESC) ): break # escape character
 
-    if(player1 == player2):
+    player2 = input("PLAYER 2: What is your play? Enter rock, paper or scissors. ")
+    if( player2Test(ESC) ): break # escape character
+
+    if( player1Test(player2) ):
         print('Draw.')
         continue
 
-    if( (player1 or player2) == 'rock'):
+    if( player1Test('rock') or player2Test('rock') ):
         #rock beats scissors
-        if(player1 == 'scissors'):
+        if( player1Test('scissors') ):
             print('You won PLAYER 2')
-        elif(player2 == 'scissors'):
+            continue
+        elif( player2Test('scissors') ):
             print('You won PLAYER 1')
-    elif( (player1 or player2) == 'scissors'):
+            continue
+    
+    if( player1Test('scissors') or player2Test('scissors') ):
         #Scissors beats paper
-        if(player1 == 'paper'):
+        if( player1Test('paper') ):
             print('You won PLAYER 2')
-        elif(player2 == 'paper'):
+            continue
+        elif( player2Test('paper') ):
             print('You won PLAYER 1')
-    elif( (player1 or player2) == 'paper'):
+            continue
+    
+    if( player1Test('paper') or player2Test('paper') ):
         #Paper beats rock
-        if(player1 == 'rock'):
+        if( player1Test('rock')):
             print('You won PLAYER 2')
-        elif(player2 == 'rock'):
+            continue
+        elif( player2Test('rock') ):
             print('You won PLAYER 1')
-        else:
-            print('PLAYER 1 = ', player1, 'PLAYER 2 = ', player2)
+            continue
+    
+    print('PLAYER 1 = ', player1, 'PLAYER 2 = ', player2)
 
 print( 'All done.' )
