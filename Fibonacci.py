969549e8b319f02e5899c057e5a2d63e94dcf155
Zeynin Juma <zeynin@hotmail.com>
2020-10-09 05:06:53 +0000

    completed

---
+++ Fibonacci.py
@@ -0,0 +1,15 @@
+# http://www.practicepython.org/exercise/2014/04/30/13-fibonacci.html
+
+def fibonacci(max=10):
+    a = [1,1]
+
+    for i in range(1,max):
+        #a.append(i)
+        next = a[i] + a[i-1]
+        #print(i,a[i],a[i-1], next)
+        a.append(next)
+       # i += 1
+ 
+    return a
+
+print(fibonacci())
