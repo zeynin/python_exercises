c69a825967f2ed39500053421e4bd6c92ade1ad4
Zeynin Juma <zeynin@hotmail.com>
2020-10-13 02:24:44 +0000

    90% just need to reverse the words, not the letters from an input now.

---
+++ Reverse Word Order.py
@@ -0,0 +1,10 @@
+# http://www.practicepython.org/exercise/2014/05/21/15-reverse-word-order.html
+
+strings = input('Give me a phrase: ')#['this', 'is', 'my', 'name']
+
+def reverseList():
+    
+    for i in range(1, len(strings) + 1):
+        print(strings[i * -1])
+
+reverseList()
