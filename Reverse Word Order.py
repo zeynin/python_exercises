a4ebf658f7c13c3a6cba26c0ae08378f41fc3e2f
Zeynin Juma <zeynin@hotmail.com>
2020-10-13 02:59:49 +0000

    cleaned up

--- Reverse Word Order.py
+++ Reverse Word Order.py
@@ -1,14 +1,15 @@
 # http://www.practicepython.org/exercise/2014/05/21/15-reverse-word-order.html
 
-strings = input('Give me a phrase: ')#.split() 
+strings = input('Give me a phrase: ') 
 
 def reverseList(asIs=['this', 'is', 'my', 'name']):
-    #return ' '.join(w.split()[::-1])
-    #reverse = [i for i in asIs.split()[::-1]]
-    #return reverse
-    return [i for i in asIs.split()[::-1]]
-    
+    #strings.split()
     #for i in range(1, len(strings) + 1):
     #    print(strings[i * -1])
-        
+
+    #solution
+    #return ' '.join(w.split()[::-1])
+ 
+    return [i for i in asIs.split()[::-1]]
+         
 print(reverseList(strings))
