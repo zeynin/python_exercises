69275950a3919c7d4a8a7bb45cb4e74345dcba1e
Zeynin Juma <zeynin@hotmail.com>
2020-10-19 03:21:06 +0000

    working - not complete

--- DecodeAWebPage.py
+++ DecodeAWebPage.py
@@ -1,17 +1,15 @@
 #http://www.practicepython.org/exercise/2014/06/06/17-decode-a-web-page.html
 
-from bs4 import BeautifulSoup
 import requests
-import webencodings
-from html5lib import html5lib
+from bs4 import BeautifulSoup
 
 
 url = 'http://www.nytimes.com/'
 r = requests.get(url)
-r_html = r.text
 
 # some requests code here for getting r_html 
 
-soup = BeautifulSoup(r_html,'features="html5lib"')
-title = soup.find('span', 'articletitle').string
-print(soup.get_text())
+soup = BeautifulSoup(r.text, 'html.parser')
+
+for title in soup.titles
+    print('title = ' + title)
